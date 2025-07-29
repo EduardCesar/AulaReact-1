8119201073a6b69841392fd343ebc9b16742b13a
EduardCesar <Gameredusardo@gmail.com>
2025-07-29 12:22:46 -0300

    29/07

--- estudocss/src/App.jsx
+++ estudocss/src/App.jsx
@@ -1,9 +1,40 @@
+import "./App.css";
+
 export default function App()
 {
+    const estilo = {
+        color : "red",
+        textAlign : "center",
+        backgroundColor : "blue",
+        padding : "10px",
+    };
+
     return (
-        <div>
+        <div className="conteudo">
             <h1>Estudo CSS</h1>
             
+            <p>
+                O CSS (<i>Cascading Style Sheets</i>) é uma linguagem
+                utilizada para estilizar as <b>páginas web</b>. Com ela
+                podemos definir:
+            </p>
+
+            <ul>
+                <li>Cores de fundo, do texto e dos elementos</li>
+                <li>Fontes e tamanhos das letras</li>
+                <li>Posicionamento dos elementos na página</li>
+                <li>Espaçamento, bordas, sombras, etc.</li>
+                <li>Efeito Visuais e animações</li>
+            </ul>
+
+            <h3 style={estilo}>Babis, o Principe do inferno</h3>
+
+            <p className="centro">
+
+
+                <img src="images.jpg" alt="" />
+            </p>
+
         </div>
     );
 }
